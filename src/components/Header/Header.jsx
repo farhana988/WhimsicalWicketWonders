92ba6e5c92d3c32dda9f76200e6b504e83cbee27
farhana988 <farhana744255@gmail.com>
2024-10-24 22:07:04 +0600

    banner

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,12 @@
 import logo from '../../assets/images/logo.png'
 import coin from '../../assets/images/coin.png'
+import bgImage from '../../assets/images/bgShadow.png'
+import banner from '../../assets/images/banner-main.png'
+
 
 export default function Header() {
   return (
-    <div className='container mx-auto mt-8'>
+    <div className='container mx-auto my-8'>
         {/* navbar */}
         <nav className='flex justify-between items-center '>
             <img src={logo} alt="" />
@@ -20,11 +23,26 @@ export default function Header() {
         </nav>
 
 
-    {/* banner */}
+        {/* banner */}
 
-    <section>
-        
-    </section>
+        <section className='py-12 mt-6 bg-cover rounded-2xl bg-[#161515] ' 
+            style={{backgroundImage: `url(${bgImage})`}}>
+            <div className='flex flex-col items-center gap-5' 
+            >
+                <img src={banner} alt="" />
+                <h1 className=' text-[#FFFFFF] font-extrabold text-4xl'>
+                    Assemble Your Ultimate Dream 11 Cricket Team
+                </h1>
+                <p className='text-[#FFFFFFB3] font-medium text-2xl'>
+                    Beyond Boundaries Beyond Limits
+                </p>
+                <div className='border-2 border-[#E7FE29] p-3 rounded-3xl'>
+                <button className='bg-[#E7FE29] text-[#131313] font-bold text-lg p-5 rounded-2xl' >
+                    Claim Free Credit
+                </button>
+                </div>
+            </div>
+        </section>
 
     </div>
 
