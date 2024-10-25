8ce1bca62adffdec7baa05d9df6ba576640e1811
farhana988 <farhana744255@gmail.com>
2024-10-25 23:59:53 +0600

    fixes

--- src/components/Select/Select.jsx
+++ src/components/Select/Select.jsx
@@ -3,19 +3,20 @@ import PropTypes from 'prop-types'
 import { RiDeleteBin5Line } from "react-icons/ri";
 
 function Select({select, handleRemove}) {
-    const {image, name, battingType}=select
+    const {image, name, battingType ,biddingPrice}=select
   return (
    
-        <div className="flex p-8 rounded-2xl items-center border-2 border-dotted
+        <div className="flex p-3 pr-10 rounded-2xl items-center border-2 border-dotted
         justify-between container mx-auto my-8">
             <div className='flex gap-5 items-center'>
                 <img 
-                className='w-20 rounded-2xl
+                className='w-32 h-28 rounded-2xl
                   '
                 src={image} alt={name} />
                 <div className='space-y-2'>
                     <h2 className='font-bold text-2xl'>{name}</h2>
-                    <p className='text-[#13131399] text-base'>{battingType}</p>
+                    <p className='text-[#13131399] text-base'>Role: {battingType}</p>
+                    <p className='text-[#13131399] text-base'>Price: ${biddingPrice}</p>
                 </div>
             </div>
             {/* delete button */}
